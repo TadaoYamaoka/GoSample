a9bd19b74f0684f19d3d3ac5465a953ad62374a8
TadaoYamaoka <tadaoyamaoka@gmail.com>
2016-04-05 00:08:13 +0900

    マウス操作追加

---
+++ Human.cpp
@@ -0,0 +1,35 @@
+#include <Windows.h>
+#include "Human.h"
+
+XY Human::select_move(Board& board, Color color)
+{
+	while (xy < 0)
+	{
+		Sleep(100);
+		// メッセージ処理
+		MSG msg;
+		while (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				return -1;
+			}
+			if (GetMessage(&msg, NULL, 0, 0))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			else {
+
+			}
+		}
+	}
+	XY ret = xy;
+	xy = -1;
+	return ret;
+}
+
+void Human::set_xy(XY xy)
+{
+	this->xy = xy;
+}
